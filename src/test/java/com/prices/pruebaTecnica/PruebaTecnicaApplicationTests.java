a38ea2370dff6cf750e75573fe5bbe2fca4189b8
Lozape <lozape97@gmail.com>
2025-11-07 15:07:13 +0100

    Primer Commit

---
+++ src/test/java/com/prices/pruebaTecnica/PruebaTecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prices.pruebaTecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
