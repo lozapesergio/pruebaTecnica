348c525d0436b5ce653c2c79d1d00813470a0b9b
Lozape <lozape97@gmail.com>
2025-11-10 18:19:32 +0100

    Control de excepciones y logs

--- src/main/java/com/prices/pruebaTecnica/application/PriceService.java
+++ src/main/java/com/prices/pruebaTecnica/application/PriceService.java
@@ -1,8 +1,10 @@
 package com.prices.pruebaTecnica.application;
 
+import com.prices.pruebaTecnica.domain.exception.NotFoundException;
 import com.prices.pruebaTecnica.domain.model.Price;
 import com.prices.pruebaTecnica.infraestructure.adapter.out.PriceRepository;
 import com.prices.pruebaTecnica.domain.dto.PriceResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -12,6 +14,7 @@ import java.util.Optional;
  * @Author - Sergio Lozano Perez
  * Servicio que se encarga de realizar la llamada al repo
  */
+@Slf4j
 @Service
 public class PriceService {
 
@@ -21,15 +24,20 @@ public class PriceService {
         this.repo = repo;
     }
 
-    public Optional<PriceResponse> getPrice(LocalDateTime applicationDate, int productId, int brandId) {
-        var results = repo.findProduct(applicationDate, productId, brandId);
+    public PriceResponse getPrice(LocalDateTime date, int productId, int brandId) {
+        log.info("Consultando precio para productId={}, brandId={}, date={}", productId, brandId, date);
 
-        if (results.isEmpty()) {
-            return Optional.empty();
-        }
-
-        Price p = results.get(0);
-        var dto = new PriceResponse(p.getProductId(), p.getBrandId(), p.getPriceList(), p.getStartDate(), p.getEndDate(), p.getPrice(), p.getCurr());
-        return Optional.of(dto);
+        return repo.findProduct(date, productId, brandId)
+                .stream()
+                .findFirst()
+                .map(p -> {
+                    log.info("Precio encontrado: priceList={}, price={}", p.getPriceList(), p.getPrice());
+                    return new PriceResponse(p.getProductId(), p.getBrandId(), p.getPriceList(),
+                            p.getStartDate(), p.getEndDate(), p.getPrice(), p.getCurr());
+                })
+                .orElseThrow(() -> new NotFoundException(
+                        String.format("No se encontró precio para el producto %d, cadena %d en la fecha %s",
+                                productId, brandId, date)
+                ));
     }
 }
